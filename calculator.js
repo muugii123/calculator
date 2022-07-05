837647121b6f9beb9c93cdc2e9a3bef6c50ba22a
muugii123 <bmunkhtuvshin22@gmail.com>
2022-07-05 20:01:31 +0800

    Added calculator

--- calculator.js
+++ calculator.js
@@ -1,8 +1,33 @@
 
-const input = document.getElementById('input')
+const input = document.getElementById('input');
+var firstNumber;
+var mathTemdeg;
+
+function displays(temdeg){
+    firstNumber=input.value;
+    mathTemdeg = temdeg;
+    input.value="";
+}
+
+function display(tovch, mathematical){
+    // if(mathematical){
+    //     firstNumber = input.value;
+    //     input.value="";
+    // }else{
+        // }
+    input.value+=tovch;
+}
+
+function Clear(){
+    // firstNumber=''
+    input.value='';
+}
+
+function DEL(){
+    input.value = input.value.slice(0, input.value.length-1)
+}
 
 
-// function display(tovch){
-
-// }
-
+function Calculate(){
+    input.value=eval(firstNumber+mathTemdeg+input.value)
+}
